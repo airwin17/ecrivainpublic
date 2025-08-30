ce842de7e38257775dd0e6fb39177a95dd12e50a
airwin67 <faitourdja@gmail.com>
2025-08-30 19:48:54 +0100

    ff

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.use(express.static( 'public' ));
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+'/public/html/home.html');
 })
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+app.listen(80,()=>{
+    console.log('Server is running on port 80');
 });
